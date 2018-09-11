b5d896deb3cd4207b7adaadafb4cab185f69d91e
zhaoguangjian2 <zhaoguangjian2@ainirobot.com>
2018-09-11 14:40:26 +0800

    增加Server和Client端的writeInt32()和writeString8()的Binder传输方法

--- BinderCallBack/binder_callback/client.cpp
+++ BinderCallBack/binder_callback/client.cpp
@@ -4,11 +4,15 @@
 #include <binder/ProcessState.h>
 #include <binder/IPCThreadState.h>
 #include <private/binder/binder_module.h>
- 
+#include <binder/IInterface.h>
+#include <binder/Parcel.h>
+#include <binder/Binder.h>
+
 using namespace android;
 #ifdef LOG_TAG
 #undef LOG_TAG
 #endif
+
 #define LOG_TAG "sampleCallback"
 #define SAMPLE_SERIVCE_DES "sample.hello"
 #define SAMPLE_CB_SERIVCE_DES "android.os.SampleCallback"
@@ -19,28 +23,47 @@ class SampeCallback : public BBinder
 public:
   SampeCallback()
   {
+    ALOGE("Client ------------------------------ %d",__LINE__);
     mydescriptor = String16(SAMPLE_CB_SERIVCE_DES);
   }
   virtual ~SampeCallback() {
-          
   }
   virtual const String16& getInterfaceDescriptor() const{
     return mydescriptor;
   }
 protected:
-     
-  void callbackFunction() {
-    ALOGI( "Client callback function called-----------cb" );
+
+  void callbackFunction(int val) {
+    ALOGE("Client -----------回调Client成功------------------- %d",__LINE__);
+    ALOGI( "Client: %s(), %d, val = %d",__FUNCTION__,__LINE__,val);
   }
-     
-  virtual status_t onTransact( uint32_t code,
-			       const Parcel& data,Parcel* reply,uint32_t flags = 0){
-    ALOGD( "Client onTransact, code = %d", code);   
-         
+
+  virtual status_t onTransact( uint32_t code, const Parcel& data,Parcel* reply,uint32_t flags = 0){
+    ALOGD( "Client onTransact, line = %d, code = %d",__LINE__,code);
+    int val_1,val_2,val_3;
+    String8 str_1,str_2,str_3;
     switch (code){
     case CB_CODE:
-      callbackFunction();
+      //1.读取int32类型数据
+      val_1 = data.readInt32();
+      val_2 = data.readInt32();
+      val_3 = data.readInt32();
+      ALOGE("Client ------------------------------ %d, read int32 = %d",__LINE__,val_1);
+      ALOGE("Client ------------------------------ %d, read int32 = %d",__LINE__,val_2);
+      ALOGE("Client ------------------------------ %d, read int32 = %d",__LINE__,val_3);
+
+
+      //2.读取String8类型字符串;str_1.string()-->String8转换char类型数组
+      // str_1 = data.readString8();
+      // str_2 = data.readString8();
+      // str_3 = data.readString8();
+      // ALOGE("Client ------------------------------ %d, read String = %s",__LINE__,str_1.string());
+      // ALOGE("Client ------------------------------ %d, read String = %s",__LINE__,str_2.string());
+      // ALOGE("Client ------------------------------ %d, read String = %s",__LINE__,str_3.string());
+
+      callbackFunction(1234567);
       break;
+
     default:
       return BBinder::onTransact(code, data, reply, flags);
     }
@@ -69,4 +92,3 @@ int main()
      while(1);
      return 0;
 }
-
--- BinderCallBack/binder_callback/server.cpp
+++ BinderCallBack/binder_callback/server.cpp
@@ -3,7 +3,7 @@
 #include <binder/Parcel.h>
 #include <binder/ProcessState.h>
 #include <binder/IPCThreadState.h>
- 
+
 using namespace android;
 #ifdef LOG_TAG
 #undef LOG_TAG
@@ -18,35 +18,51 @@ using namespace android;
 class SampleService: public BBinder {
 public:
   SampleService() {
+    ALOGE("Server ------------------------------ %d",__LINE__);
     mydescriptor = String16(SAMPLE_SERIVCE_DES);
   }
-     
+
   virtual ~SampleService() {
   }
 
   virtual const String16& getInterfaceDescriptor() const {
     return mydescriptor;
   }
-     
+
 protected:
-     
-  void callFunction() {
-    ALOGE( "Service callFunction-----------");
+
+  void callFunction(int val) {
+    ALOGE("Server ------------------------------ %d",__LINE__);
+    ALOGI( "Service: %s(), %d, val = %d",__FUNCTION__,__LINE__,val);
   }
-     
-  virtual status_t onTransact(uint32_t code, const Parcel& data,
-			      Parcel* reply, uint32_t flags = 0) {
-    ALOGD( "Service onTransact, code = %d" , code);
+
+  virtual status_t onTransact(uint32_t code, const Parcel& data, Parcel* reply, uint32_t flags = 0) {
+    ALOGD( "Service onTransact,line = %d, code = %d",__LINE__, code);
     switch (code) {
     case SRV_CODE:
+
       callback = data.readStrongBinder();
-      if (callback != NULL)
-	{
-	  Parcel _data, _reply;
-	  _data.writeInterfaceToken(String16(SAMPLE_CB_SERIVCE_DES));
-	  int ret = callback->transact(CB_CODE, _data, &_reply, 0);
-	}
-      callFunction();
+
+      if(callback != NULL)
+      {
+         Parcel _data, _reply;
+         //_data.writeInterfaceToken(String16(SAMPLE_CB_SERIVCE_DES));
+
+	 //3.int32类型,每次传输只能传一种数据类型,不能同时传String和int类型,但是可以传递多个值
+	 //写入数据通过Parcel传递给client端,用readInt32()读出来即可.
+	 _data.writeInt32(7777777);
+	 _data.writeInt32(8888888);
+	 _data.writeInt32(9999999);
+
+	 //2.String8类型,每次传输只能传一种数据类型,不能同时传String和int类型,但是可以传递多个值
+	 // _data.writeString8(String8("Hello..."));
+	 // _data.writeString8(String8("are..."));
+	 // _data.writeString8(String8("you..."));
+	 //回调客户端
+	 int ret = callback->transact(CB_CODE, _data, &_reply, 0);
+       }
+      //调用server端的
+      callFunction(5555);
       break;
     default:
       return BBinder::onTransact(code, data, reply, flags);
