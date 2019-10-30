5fb49faca779c9ec0eb753d2b5ac5faa59aaf67f
MeekJeen <zgj224@163.com>
2019-10-30 17:55:30 +0800

    Update Android.mk

--- AudioRecord_Demo/Android.mk
+++ AudioRecord_Demo/Android.mk
@@ -6,7 +6,7 @@ LOCAL_CERTIFICATE := platform
 #LOCAL_MODULE_TAGS := tests
 LOCAL_PACKAGE_NAME := AudioDemo
 LOCAL_DEX_PREOPT := false
-LOCAL_SDK_VERSION := 21
+#LOCAL_SDK_VERSION := 21
 
 LOCAL_SRC_FILES := $(call all-java-files-under, java)
 include $(BUILD_PACKAGE)
